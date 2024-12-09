87365d43c753f84e1295cccf22689581b443b73c
Peam <peammy2010@hotmail.com>
2024-12-09 11:33:35 +0700

    chore: change git author

--- sync.sh
+++ sync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
-export GIT_COMMITTER_NAME=dtinth-bot
-export GIT_AUTHOR_NAME=dtinth-bot
-export GIT_COMMITTER_EMAIL=dtinth-bot@users.noreply.github.com
-export GIT_AUTHOR_EMAIL=dtinth-bot@users.noreply.github.com
+export GIT_COMMITTER_NAME=softnetics-fresh-app-bot
+export GIT_AUTHOR_NAME=softnetics-fresh-app-bot
+export GIT_COMMITTER_EMAIL=softnetics-fresh-app-bot@users.noreply.github.com
+export GIT_AUTHOR_EMAIL=softnetics-fresh-app-bot@users.noreply.github.com
 
 cd workspace/fresh-app
 rm -rf .git
